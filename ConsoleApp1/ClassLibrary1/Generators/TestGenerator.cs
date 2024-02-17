c7c867ed9643576d61ebb750f8562d63bc648e41
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-17 08:01:36 +0100

    Update

--- ConsoleApp1/ClassLibrary1/Generators/TestGenerator.cs
+++ ConsoleApp1/ClassLibrary1/Generators/TestGenerator.cs
@@ -8,30 +8,23 @@ namespace ClassLibrary1.Generators
     [Generator(LanguageNames.CSharp)]
     public class TestGenerator : IIncrementalGenerator
     {
-        public void Initialize(IncrementalGeneratorInitializationContext initContext)
+        public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            // define the execution pipeline here via a series of transformations:
-
-            // find all additional files that end with .txt
-            IncrementalValuesProvider<AdditionalText> textFiles =
-                initContext.AdditionalTextsProvider;
-
-            // read their contents and save their name
-            IncrementalValuesProvider<(string name, string content)> namesAndContents =
-                textFiles.Select(
-                    (text, cancellationToken) =>
-                        (
-                            name: Path.GetFileNameWithoutExtension(text.Path),
-                            content: text.GetText(cancellationToken)!.ToString()
-                        )
-                );
+            var codebuilders = context.CompilationProvider.Select(
+                static (compilation, _) =>
+                {
+                    return "";
+                }
+            );
 
             // generate a class that contains their values as const strings
-            initContext.RegisterPostInitializationOutput(spc =>
-            {
-                spc.AddSource(
-                    $"Test2.cs",
-                    $@"
+            context.RegisterSourceOutput(
+                codebuilders,
+                static (sourceProductionContext, codeBuildersTuples) =>
+                {
+                    sourceProductionContext.AddSource(
+                        $"Test2.cs",
+                        $@"
     using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,8 +38,9 @@ namespace ConsoleApp1
     }}
 }}
 "
-                );
-            });
+                    );
+                }
+            );
         }
     }
 }
